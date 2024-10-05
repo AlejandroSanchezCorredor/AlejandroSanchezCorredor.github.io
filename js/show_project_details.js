c30e6898414b7545a5de8f2af2e2da65dffb7815
ALEJANDRO SÁNCHEZ CORREDOR <Alejandro.Sanchez51@alu.uclm.es>
2024-10-05 15:51:25 +0200

    Fix bugs

--- js/show_project_details.js
+++ js/show_project_details.js
@@ -149,7 +149,7 @@ function showProjectDetails(project) {
 
             <!-- Botones para descargar los PDFs -->
             <div class="download-buttons">
-                <a href="prediccion_noticias/Libreta_Alejandro_Sánchez_FakeNews.ipynb.ipynb" class="btn-small" download>Descargar libreta</a>
+                <a href="prediccion_noticias/Libreta_Alejandro_Sánchez_FakeNews.ipynb" class="btn-small" download>Descargar libreta</a>
                 <a href="https://drive.google.com/file/d/11tfGCD0clE-VrafI63FcgdmbsD8iTcFu/view?usp=drive_link" class="btn-small" download>Descargar Proyecto</a>
             </div>
 
